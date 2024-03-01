edd92b037c944e7a6829cc6bd17baa5b7de0960f
Guilherme Duarte <guilhermealduarte@gmail.com>
2024-03-01 15:04:33 -0300

    Implementation Exception Handler Controllers

---
+++ src/main/java/com/guilhermeduarte/projectecommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.guilhermeduarte.projectecommerce.controllers.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.guilhermeduarte.projectecommerce.dto.error.CustomError;
+import com.guilhermeduarte.projectecommerce.dto.error.ValidationError;
+import com.guilhermeduarte.projectecommerce.services.exceptions.DatabaseException;
+import com.guilhermeduarte.projectecommerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		CustomError error = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError error = new ValidationError(Instant.now(), status.value(), "Dados Inválidos", request.getRequestURI());
+		
+		for(FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(status).body(error);
+	}
+}
