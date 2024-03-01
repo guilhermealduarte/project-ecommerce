fc724b0c033e5147cb4adab98abb60d8f8c6650a
Guilherme Duarte <guilhermealduarte@gmail.com>
2024-03-01 14:56:22 -0300

    Create Entities

---
+++ src/main/java/com/guilhermeduarte/projectecommerce/entities/Order.java
@@ -0,0 +1,121 @@
+package com.guilhermeduarte.projectecommerce.entities;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant moment;
+	
+	private OrderStatus status;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
+	
+	public Order() {
+		
+	}
+
+	public Order(Long id, Instant moment, OrderStatus status, User user, Payment payment) {
+		super();
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.user = user;
+		this.payment = payment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts(){
+		return items.stream().map(x -> x.getProduct()).toList();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ src/main/java/com/guilhermeduarte/projectecommerce/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.guilhermeduarte.projectecommerce.entities;
+
+public enum OrderStatus {
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED;
+}
