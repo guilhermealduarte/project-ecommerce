43602579c2765f08ad393ed9de3f7fdcfbbe9877
Guilherme Duarte <guilhermealduarte@gmail.com>
2024-03-01 14:55:13 -0300

    Create Project

---
+++ src/test/java/com/guilhermeduarte/projectecommerce/ProjectEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeduarte.projectecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
