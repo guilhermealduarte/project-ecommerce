a94772d2b8e7391b382ca56a86fb974cedd33fd7
Guilherme Duarte <guilhermealduarte@gmail.com>
2024-03-04 08:36:46 -0300

    Implementation GrantedAuthority

--- src/main/java/com/guilhermeduarte/projectecommerce/entities/User.java
+++ src/main/java/com/guilhermeduarte/projectecommerce/entities/User.java
@@ -2,20 +2,30 @@ package com.guilhermeduarte.projectecommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +41,10 @@ public class User {
 	private LocalDate birthDate;
 	
 	private String password;
+	
+	@ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 	
 	@OneToMany(mappedBy = "user")
 	private List<Order> orders = new ArrayList<>();
@@ -101,6 +115,23 @@ public class User {
 		return orders;
 	}
 
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	
+	public void addRole(Role role) {
+    	roles.add(role);
+    }
+    
+    public boolean hasRole(String roleName) {
+    	for(Role role: roles) {
+    		if(role.getAuthority().equals(roleName)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -116,5 +147,41 @@ public class User {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles;
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		
+		return true;
 	}
 }
