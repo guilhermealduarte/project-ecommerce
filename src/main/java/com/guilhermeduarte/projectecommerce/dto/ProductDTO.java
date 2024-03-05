9e64f29964f5b9d915ba3beb127d9679d7c9acbf
Guilherme Duarte <guilhermealduarte@gmail.com>
2024-03-05 11:13:55 -0300

    Fix validation

--- src/main/java/com/guilhermeduarte/projectecommerce/dto/ProductDTO.java
+++ src/main/java/com/guilhermeduarte/projectecommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.guilhermeduarte.projectecommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -23,6 +24,7 @@ public class ProductDTO {
 	@NotBlank(message = "Campo descrição é obrigatório")
 	private String description;
 	
+	@NotNull(message = "O campo preço é requerido")
 	@Positive(message = "O preço deve ser positivo")
 	private Double price;
 	
